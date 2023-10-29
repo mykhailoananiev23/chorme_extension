7a7bd580be93ab61e6f43c09d82fb1a4dbd37e23
mykhailoananiev23 <mykhailoananiev23@gmail.com>
2023-10-29 02:30:44 +0200

    updated workflow

--- scripts/content.js
+++ scripts/content.js
@@ -9,37 +9,57 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   }
 });
 
-var select = document.getElementById("post_select");
-
-var textToMatch = "CHENNAI VAC";
-
-// Loop through the options to find the one with the matching text
-for (var i = 0; i < select.options.length; i++) {
-  if (select.options[i].text === textToMatch) {
-    // Set the selectedIndex to the found option's index
-    select.selectedIndex = i;
-    break; // Exit the loop once a match is found
-  }
-}
-
-
-var openCalendar = document.getElementById("ui-datepicker-div")
-
 setTimeout(function () {
-  console.log("openCalendar")
-  openCalendar.style.display = "block !important";
-}, 10000)
+  var select = document.getElementById("post_select");
 
-var calender_mon_next = document.querySelector('[data-handler="next"]');
+  var textToMatch = "CHENNAI VAC";
 
-setTimeout(function () {
-  if(calender_mon_next){
-    calender_mon_next.addEventListener("click", function() {
-      // Your code to handle the "next" click goes here
-      console.log("Button with data-handler='next' clicked");
-    });
-  
-    // Programmatically trigger the click event
-    calender_mon_next.click();
+  // Loop through the options to find the one with the matching text
+  if (select) {
+    for (var i = 0; i < select.options.length; i++) {
+      if (select.options[i].text === textToMatch) {
+        // Set the selectedIndex to the found option's index
+        select.selectedIndex = i;
+        break; // Exit the loop once a match is found
+      }
+    }
   }
-}, 15000)
+  setTimeout(function () {
+    var openCalendar = document.getElementById("datepicker");
+    openCalendar && openCalendar.focus();
+    setTimeout(function () {
+      var rollCount = 0;
+      var matchedDate;
+      function matchData() {
+        rollCount++;
+        var calender_mon_next = document.querySelector('[data-handler="next"]');
+        var days = document.querySelector(".ui-datepicker-calendar");
+        var trs = days.childNodes[1].childNodes;
+        for (let i = 0; i < trs.length; i++) {
+          var tds = trs[i].childNodes;
+          if (rollCount > 50) {
+            break;
+          } else {
+            for (let j = 0; j < tds.length; j++) {
+              const element = tds[j];
+              if (
+                element.classList.length > 0 &&
+                element.classList.contains("greenday")
+              ) {
+                matchedDate = element;
+                rollCount = 51;
+              } else {
+                if (calender_mon_next) {
+                  calender_mon_next.click();
+                  matchData();
+                }
+              }
+            }
+          }
+        }
+      }
+      matchData();
+      console.log(matchedDate);
+    }, 3000);
+  }, 4000);
+}, 2000);
